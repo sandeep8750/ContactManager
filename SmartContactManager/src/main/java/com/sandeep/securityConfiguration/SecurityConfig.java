af0e8a835679ea3f4bbadb3fb083a60b87bf01e8
sandeep8750 <monu875036@gmail.com>
2024-06-16 22:18:40 +0530

    second commit

--- SmartContactManager/src/main/java/com/sandeep/securityConfiguration/SecurityConfig.java
+++ SmartContactManager/src/main/java/com/sandeep/securityConfiguration/SecurityConfig.java
@@ -1,70 +1,100 @@
 package com.sandeep.securityConfiguration;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sound.midi.Soundbank;
-
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
+    // user create and login using java code with in memory service
 
-    private SecurityCustomUserDetailService userDetailService;
+    // @Bean
+    // public UserDe    tailsService userDetailsService() {
 
+    // UserDetails user1 = User
+    // .withDefaultPasswordEncoder()
+    // .username("admin123")
+    // .password("admin123")
+    // .roles("ADMIN", "USER")
+    // .build();
 
-    private BCryptPasswordEncoder passwordEncoder;
+    // UserDetails user2 = User
+    // .withDefaultPasswordEncoder()
+    // .username("user123")
+    // .password("password")
+    // // .roles(null)
+    // .build();
+
+    // var inMemoryUserDetailsManager = new InMemoryUserDetailsManager(user1,
+    // user2);
+    // return inMemoryUserDetailsManager;
+
+    // }
 
     @Autowired
-    public SecurityConfig(SecurityCustomUserDetailService userDetailService, BCryptPasswordEncoder passwordEncoder) {
-        this.userDetailService = userDetailService;
-        this.passwordEncoder = passwordEncoder;
-    }
+    private SecurityCustomUserDetailService userDetailService;
 
-
+    // configuraiton of authentication providerfor spring security
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests((httpRequest) -> {
-            httpRequest.requestMatchers("/home", "/register", "/service").permitAll();
-            httpRequest.requestMatchers("/user/**").authenticated();
-            httpRequest.anyRequest().permitAll();
-        });
-
-        httpSecurity.formLogin((formlogin) -> {
-            formlogin.loginPage("/login");
-            formlogin.loginProcessingUrl("/authenticate");
-            formlogin.defaultSuccessUrl("/user/dashboard", true);
-            formlogin.failureUrl("/login?error=true");
-            formlogin.usernameParameter("email");
-            formlogin.passwordParameter("password");
-        });
-
-        httpSecurity.logout((logout) -> {
-            logout.logoutUrl("/logout");
-            logout.logoutSuccessUrl("/home");
-            logout.invalidateHttpSession(true);
-            logout.deleteCookies("JSESSIONID");
-        });
-
-
-        return httpSecurity.build();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
+    public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        System.out.println("sandeep kumar prajapati");
+        // user detail service ka object:
         daoAuthenticationProvider.setUserDetailsService(userDetailService);
-        System.out.println(userDetailService.toString());
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        // password encoder ka object
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
         return daoAuthenticationProvider;
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        // configuration
+        httpSecurity.authorizeHttpRequests(authorize -> {
+            authorize.requestMatchers("/home", "/register", "/services").permitAll();
+            authorize.requestMatchers("/user/**").authenticated();
+            authorize.anyRequest().permitAll();
+        });
+
+        httpSecurity.formLogin(formLogin -> {
+            formLogin.loginPage("/login");
+            formLogin.loginProcessingUrl("/authenticate");
+            formLogin.successForwardUrl("/user/profile");
+            // formLogin.failureForwardUrl("/login?error=true");
+            // formLogin.defaultSuccessUrl("/home");
+            formLogin.usernameParameter("email");
+            formLogin.passwordParameter("password");
+        });
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        // oauth configurations
+
+//        httpSecurity.oauth2Login(oauth -> {
+//            oauth.loginPage("/login");
+//            oauth.successHandler(handler);
+//        });
+
+        httpSecurity.logout(logoutForm -> {
+            logoutForm.logoutUrl("/do-logout");
+            logoutForm.logoutSuccessUrl("/login?logout=true");
+        });
+
+        return httpSecurity.build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
+
